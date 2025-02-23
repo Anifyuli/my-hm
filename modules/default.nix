4ec0c583f2b3558ad9a44158cef6a278beaccdd2
Anifyuli <anifyuliansyah@gmail.com>
2025-02-23 21:46:51 +0700

    fix: auto import configs

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, lib, ... }:
-
+{ lib, ... }:
 let
   folder = builtins.path { path = ./.; };
   toImport = name: value: folder + ("/" + name);
-  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
-  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+  filterConf = key: value: value == "regular" && lib.hasSuffix ".nix" key && key != "default.nix";
+  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterConf (builtins.readDir folder));
 in {
   inherit imports;
 }
+
