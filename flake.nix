8ee2be9ad2f332442f9a26f06b2d4551e9145e69
Anifyuli <anifyuliansyah@gmail.com>
2025-07-10 19:37:01 +0700

    chore: change follow inputs nixgl to nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,20 +7,30 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixgl.url = "github:nix-community/nixGL";
-  };
-
-  outputs = { nixpkgs, home-manager, nixgl, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    homeConfigurations."anifyuli" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit nixgl; };
-      modules = [ 
-        ./home.nix 
-      ];
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nixgl,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."anifyuli" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit nixgl; };
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
 }
