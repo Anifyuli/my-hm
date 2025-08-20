527f16786ba6a9b78652f72c5dd7d462e76228f2
Anifyuli <anifyuliansyah@gmail.com>
2025-08-20 21:05:59 +0700

    fix: try other behavior & formatting fish config

--- options/fish/default.nix
+++ options/fish/default.nix
@@ -14,36 +14,38 @@ in
       fish_user_key_bindings = lib.fileContents (fnDir + "/fish_user_key_bindings.fish");
     };
     interactiveShellInit = ''
-            # Ensure $HOME/.local/bin is in PATH
-            if not contains -- $HOME/.local/bin $PATH
-                set -gx PATH $HOME/.local/bin $HOME/bin $PATH
-            end
+      # Ensure $HOME/.local/bin is in PATH
+      if not contains -- $HOME/.local/bin $PATH
+          set -gx PATH $HOME/.local/bin $HOME/bin $PATH
+      end
 
-            # Set fish_greeting as the default greeting function
-            #set -g fish_greeting fish_greeting
+      # Set fish_greeting as the default greeting function
+      #set -g fish_greeting fish_greeting
 
-            # Android SDK configuration
-            set -gx ANDROID_HOME $HOME/.android/sdk
-            set -gx ANDROID_AVD_HOME $HOME/.android/avd
-            set -gx ANDROID_SDK_ROOT $ANDROID_HOME
-            set -gx PATH $PATH \
-                $ANDROID_HOME/cmdline-tools/latest/bin \
-                $ANDROID_HOME/emulator \
-                $ANDROID_HOME/platform-tools
+      # Android SDK configuration
+      set -gx ANDROID_HOME $HOME/.android/sdk
+      set -gx ANDROID_AVD_HOME $HOME/.android/avd
+      set -gx ANDROID_SDK_ROOT $ANDROID_HOME
+      set -gx PATH $PATH \
+          $ANDROID_HOME/cmdline-tools/latest/bin \
+          $ANDROID_HOME/emulator \
+          $ANDROID_HOME/platform-tools
 
-            # pnpm configuration
-            set -gx PNPM_HOME $HOME/.local/share/pnpm
-            if not contains -- $PNPM_HOME $PATH
-                set -gx PATH $PNPM_HOME $PATH
-            end
+      # pnpm configuration
+      set -gx PNPM_HOME $HOME/.local/share/pnpm
+      if not contains -- $PNPM_HOME $PATH
+          set -gx PATH $PNPM_HOME $PATH
+      end
 
-            # tmux autostart
-            if type -q tmux
-    					if not test -n "$TMUX"
-    						set n (tmux list-sessions 2>/dev/null | count)
-								exec tmux new-session -s tab-$n
-    					end
-						end
+      # tmux autostart
+      if type -q tmux
+        if not set -q TMUX
+          tmux has-session -t main 2>/dev/null
+          or tmux new-session -ds main
+          tmux new-window -t main
+          tmux attach-session -t main
+        end
+      end
     '';
     shellAliases = {
       hm = "home-manager";
