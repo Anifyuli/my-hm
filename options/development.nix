00168257349189eac099c2469b8a55d0daf9746b
Anifyuli <anifyuliansyah@gmail.com>
2025-10-08 08:34:49 +0700

    change: Zed editor regular

--- options/development.nix
+++ options/development.nix
@@ -23,7 +23,7 @@
   # Zed editor
   programs.zed-editor = {
     enable = true;
-    package = config.lib.nixGL.wrap pkgs.zed-editor-fhs_git;
+    package = config.lib.nixGL.wrap pkgs.zed-editor_git;
     extensions = [
       "html"
       "gruvbox-material"
